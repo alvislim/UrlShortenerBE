4dedb606246d71ed7847bc39ea19aacf9c8aa058
alvislim <luvhatehero1@gmail.com>
2024-05-17 13:58:00 +0800

    added generic  vercel json for cors

--- router/index.js
+++ router/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const postUrl = require("../controller/postUrl");
 const redirectUrl = require("../controller/redirectUrl");
 
-router.get("/", postUrl.healthCheck);
+// router.get("/", postUrl.healthCheck);
 router.post("/post", postUrl.postUrl);
 router.get("/:urlId", redirectUrl.redirection);
 
